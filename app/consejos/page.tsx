84720a3a12ac91cdbc46588c3dfd78c64ab43c61
tonimas35 <94178121+tonimas35@users.noreply.github.com>
2025-08-12 19:40:57 +0200

    Fix road conditions link

--- app/consejos/page.tsx
+++ app/consejos/page.tsx
@@ -4,7 +4,7 @@ export default function Consejos() {
       <h1>Consejos prácticos</h1>
       <ul>
         <li>Clima cambiante: capas, impermeable y funda para mochila.</li>
-        <li>Carreteras: revisa <a href="https://umferd.is/en">umferd.is</a> y <a href="https://safetravel.is/">SafeTravel</a>.</li>
+        <li>Carreteras: revisa <a href="https://umferdin.is/en">umferdin.is</a> y <a href="https://safetravel.is/">SafeTravel</a>.</li>
         <li>Gasolineras: planea repostajes en zonas remotas.</li>
       </ul>
     </section>
